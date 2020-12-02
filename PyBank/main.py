cb954cce4a314bc8bdca595330557ec1ac7bf032
KenSaville <45977942+KenSaville@users.noreply.github.com>
2020-12-01 22:12:23 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,25 +7,34 @@ import pandas as pd
 
 csvpath = os.path.join("resources", "pybank_budget_data.csv")
 
+
+profit_1 = []
+profit_2 = []
+
 with open (csvpath, newline="") as csvfile:
     budgetreader = csv.reader(csvfile, delimiter = ',')
     print(budgetreader)
 
-    csv_header = next(budgetreader)
-    print(f"CSV Header: {csv_header}")
+    i = 0
+    for row in budgetreader:
+        if i > 0:
+            profit_1.append(float(row[1]))
+            profit_2.append(float(row[1]))
+        i += 1    
+            
+profit_1.pop()
 
-    #for row in budgetreader:
-     #       print(row[0])
-
-df = pd.read_csv("resources/pybank_budget_data.csv")
-print(df.head(5))
-
-print(df['Date'].head(5))
-
-print(df[1].head(5))
+profit_2.pop(0)
 
 
+profit_1 = np.asarray(profit_1)
+profit_2 = np.asarray(profit_2)
 
 
+diffs = np.subtract(profit_2, profit_1)
 
+#print(profit_2)
+#print(profit_1)
+#print(diffs)
+print (round(np.mean(diffs),2))
     
