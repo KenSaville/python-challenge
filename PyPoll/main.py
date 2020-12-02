bb33f37f2db8e33e35f9af21b0b0a9ec3acac65c
KenSaville <45977942+KenSaville@users.noreply.github.com>
2020-12-02 11:18:44 -0500

    getting started.  candidate list created, votes dctionary created

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
-import numpy as np
-import pandas as pd
+#import numpy as np
+#import pandas as pd
 
 csvpath = os.path.join('..',"PyPoll","resources", "pypoll_election_data.csv")
 
@@ -9,13 +9,41 @@ with open (csvpath, newline="") as csvfile:
     pollreader = csv.reader(csvfile, delimiter = ',')
     print(pollreader)
 
-    csv_header = next(pollreader)
-    print(f"CSV Header: {csv_header}")
+#initialize empty lists
+candidates = []
 
-    #for row in pollreader:
-     #   print (row[0])
+# open pybank_budget file, create budget reader object
 
-# read dat into a dataframe using pandas
+with open (csvpath, newline="") as csvfile:
+    votereader = csv.reader(csvfile, delimiter = ',')
+    print(votereader)
 
-df = pd.read_csv("resources/pypoll_election_data.csv")
-print(df.head(5))
+# iterate through rows, skipping first (header) row. add first candidate t list,
+# then check to see if each additional candidate is in the list.  If not add it.
+    i = 0
+    for row in votereader:
+        if i == 1:
+            first_name = row[2]
+            candidates.append(first_name)
+        elif i > 1 and row[2] not in candidates:
+                candidates.append(row[2]) 
+        i += 1    
+
+print(candidates)
+
+# create dictionary with key:value = candidates:votes, with votes starting at 0.
+votes = {}
+for i in candidates:
+    votes[i] = 0
+
+print (votes)
+#write analysis to file
+
+#out_path = os.path.join("analysis", "poll_results.txt")
+
+#with open (out_path, 'w') as newfile:
+
+ #   newfile.write("------------------\n")
+  #  newfile.write ("Election Results\n")
+    
+    
